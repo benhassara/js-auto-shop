c8410f156d5d7a69096373df29f09aec59d68550
benhassara <bhassara@gmail.com>
2015-08-09 12:59:10 -0400

    define Car obj

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
 // add scripts
 
-console.log("sanity check!");
+// console.log("sanity check!");
+exports.Car = function(make, model, year, color){
+  return new Car(make, model, year, color);
+};
+
+function Car(make, model, year, color) {
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  this.color = color;
+  this.state = 'off';
+  this.previous_owners = [];
+  this.current_owner = 'manufacturer';
+}
+
+Car.prototype.sale = function(newOwner) {
+  //sell car to newOwner
+  this.previous_owners.push(this.current_owner);
+  this.current_owner = newOwner;
+};
--- spec/spec.js
+++ spec/spec.js
@@ -1,8 +1,18 @@
 var code = require('../js/main.js');
 
 
-// describe('', function() {
-//   it('', function() {
+describe('Car class', function(){
+  var test1 = code.Car('Chevrolet', 'Tahoe', 2008, 'white');
 
-//   });
-// });
+  it('should autoset state to off', function(){
+    expect(test1.state).toEqual('off');
+  });
+
+  it('should default current_owner to manufacturer', function(){
+    expect(test1.current_owner).toEqual('manufacturer');
+  });
+
+  it('should default previous_owners to empty array', function(){
+    expect(test1.previous_owners).toEqual([]);
+  });
+});
