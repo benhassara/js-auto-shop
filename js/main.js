3a9870e58ac35ac134bf5c3e344b439f0ab8126a
benhassara <bhassara@gmail.com>
2015-08-09 15:18:55 -0400

    finish Car exercises

--- js/main.js
+++ js/main.js
@@ -13,10 +13,75 @@ function Car(make, model, year, color) {
   this.state = 'off';
   this.previous_owners = [];
   this.current_owner = 'manufacturer';
+  this.passengers = [];
 }
 
 Car.prototype.sale = function(newOwner) {
   //sell car to newOwner
   this.previous_owners.push(this.current_owner);
   this.current_owner = newOwner;
+};
+
+Car.prototype.paint = function(newColor) {
+  //change the color of the car
+  this.color = newColor;
+};
+
+Car.prototype.start = function() {
+  //set Car state to on
+  this.state = 'on';
+};
+
+Car.prototype.off = function() {
+  //set Car state to off
+  this.state = 'off';
+};
+
+Car.prototype.driveTo = function(destination) {
+  //drive to destination if the Car is on
+  if (this.state === 'on') {
+    console.log('Driving to ', destination);
+    return 'Driving to ' + destination;
+  }
+  else {
+    console.log('Sorry, this car is off.');
+    return 'Sorry, this car is off.';
+  }
+};
+
+Car.prototype.park = function() {
+  //park if car state is off, else error
+  if (this.state === 'off') {
+    console.log('Parked!');
+    return 'Parked!';
+  }
+  else {
+    console.log('Sorry, the car is still on.');
+    return 'Sorry, the car is still on.';
+  }
+};
+
+Car.prototype.pickUp = function(passenger) {
+  //log driving to pick up passenger if car is on
+  if (this.state === 'on') {
+    this.passengers.push(passenger);
+    console.log('Driving to pick up ' + passenger);
+    return 'Driving to pick up ' + passenger;
+  }
+  else {
+    console.log('Sorry, this car is off.');
+    return 'Sorry, this car is off.';
+  }
+};
+
+Car.prototype.dropOff = function(passenger) {
+  //if Car is on, and passenger is in Car, remove name from passengers
+  if (this.passengers.length === 0)
+    return 'This car has no passengers.';
+  else if (this.state === 'off')
+    return 'Sorry, this car is off.';
+  else {
+    var passIndex = this.passengers.indexOf(passenger);
+    return this.passengers.splice(passIndex, 1);
+  }
 };
--- spec/spec.js
+++ spec/spec.js
@@ -16,3 +16,58 @@ describe('Car class', function(){
     expect(test1.previous_owners).toEqual([]);
   });
 });
+
+describe('methods', function(){
+  var test2 = code.Car('GMC', 'Yukon', 2007, 'black');
+  test2.sale('Billy');
+
+  it('sale() should set current_owner to newOwner', function(){
+    expect(test2.current_owner).toEqual('Billy');
+    expect(test2.previous_owners).toEqual(['manufacturer']);
+  });
+
+  test2.paint('red');
+  it('paint() should update the color of the car', function(){
+    expect(test2.color).toEqual('red');
+  });
+
+  test2.start();
+  // it('start() should set state to on', function(){
+  //   expect(test2.state).toEqual('on');
+  // });
+
+  test2.off();
+  it('off() should set state to off', function(){
+    expect(test2.state).toEqual('off');
+  });
+
+  it('driveTo() should log driving to destination if car is on', function(){
+    console.log();
+    expect(test2.driveTo('Florida')).toEqual('Sorry, this car is off.');
+    test2.start();
+    console.log();
+    expect(test2.driveTo('Florida')).toEqual('Driving to Florida');
+  });
+
+  it('park() should log Parked if car is off, otherwise error', function(){
+    console.log();
+    expect(test2.park()).toEqual('Sorry, the car is still on.');
+    test2.off();
+    console.log();
+    expect(test2.park()).toEqual('Parked!');
+  });
+
+  it('pickUp should log driving to pick up if car is on', function(){
+    console.log();
+    expect(test2.pickUp('Becky')).toEqual('Sorry, this car is off.');
+    console.log();
+    test2.start();
+    expect(test2.pickUp('Becky')).toEqual('Driving to pick up Becky');
+  });
+
+  it('dropOff should remove passenger from passengers if the car is on', function(){
+    expect(test2.passengers).toEqual(['Becky']);
+    test2.dropOff('Becky');
+    expect(test2.passengers).toEqual([]);
+  });
+});
