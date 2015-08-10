729749dab1b38ffa07ad7488d6a5fe46b724c8fd
benhassara <bhassara@gmail.com>
2015-08-10 11:57:05 -0600

    refactor, improve readability

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
-// add scripts
 
-// console.log("sanity check!");
 exports.Car = function(make, model, year, color){
   return new Car(make, model, year, color);
 };
@@ -17,69 +15,84 @@ function Car(make, model, year, color) {
 }
 
 Car.prototype.sale = function(newOwner) {
-  //sell car to newOwner
+  // sell car to newOwner
+
   this.previous_owners.push(this.current_owner);
   this.current_owner = newOwner;
 };
 
 Car.prototype.paint = function(newColor) {
-  //change the color of the car
+  // change the color of the car
+
   this.color = newColor;
 };
 
 Car.prototype.start = function() {
-  //set Car state to on
+  // set Car state to on
+
   this.state = 'on';
 };
 
 Car.prototype.off = function() {
-  //set Car state to off
+  // set Car state to off
+
   this.state = 'off';
 };
 
 Car.prototype.driveTo = function(destination) {
-  //drive to destination if the Car is on
-  if (this.state === 'on') {
-    console.log('Driving to ', destination);
-    return 'Driving to ' + destination;
-  }
-  else {
-    console.log('Sorry, this car is off.');
-    return 'Sorry, this car is off.';
-  }
+  // drive to destination if the Car is on
+
+  var output = "";
+
+  if (this.state === 'on')
+    output = 'Driving to ' + destination;
+  else
+    output = 'Sorry, this car is off.';
+
+  console.log(output);
+  return output;
 };
 
 Car.prototype.park = function() {
-  //park if car state is off, else error
-  if (this.state === 'off') {
-    console.log('Parked!');
-    return 'Parked!';
-  }
-  else {
-    console.log('Sorry, the car is still on.');
-    return 'Sorry, the car is still on.';
-  }
+  // park if car state is off, else error
+  var output = "";
+
+  if (this.state === 'off')
+    output = 'Parked!';
+  else
+    output = 'Sorry, the car is still on.';
+
+  console.log(output);
+  return output;
 };
 
 Car.prototype.pickUp = function(passenger) {
-  //log driving to pick up passenger if car is on
+  // log driving to pick up passenger if car is on
+
+  var result = "";
+
   if (this.state === 'on') {
     this.passengers.push(passenger);
-    console.log('Driving to pick up ' + passenger);
-    return 'Driving to pick up ' + passenger;
+    result = 'Driving to pick up ' + passenger;
   }
-  else {
-    console.log('Sorry, this car is off.');
-    return 'Sorry, this car is off.';
-  }
+  else
+    result = 'Sorry, this car is off.';
+
+  console.log(result);
+  return result;
 };
 
 Car.prototype.dropOff = function(passenger) {
-  //if Car is on, and passenger is in Car, remove name from passengers
+  // if Car is on, and passenger is in Car, remove name from passengers
+
+  // no passengers in Car
   if (this.passengers.length === 0)
     return 'This car has no passengers.';
+
+  // if Car is off
   else if (this.state === 'off')
     return 'Sorry, this car is off.';
+
   else {
     var passIndex = this.passengers.indexOf(passenger);
     return this.passengers.splice(passIndex, 1);
